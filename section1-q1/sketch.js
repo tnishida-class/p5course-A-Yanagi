067c6a3406ba4f089a1366bca631dc427d60efc0
A-Yanagi <aoi.yana79km@gmail.com>
2021-10-18 23:16:05 +0900

    change section1-q1

--- section1-q1/sketch.js
+++ section1-q1/sketch.js
@@ -3,12 +3,14 @@ function setup(){
   createCanvas(200, 200);
   background(255);
 
+
   // 空欄を埋めて神戸市のロゴマークを完成させよう
   noFill();
   strokeWeight(25);
   strokeCap(SQUARE);
+  stroke(0, 100, 0)
   arc(100 + 25, 100, 100, 100, QUARTER_PI * 3, QUARTER_PI * 3 + PI);
-  // BLANK[1]
+  arc(50 + 25, 100, 100, 100, QUARTER_PI * 5, QUARTER_PI * 5 + PI);
 
   // おまけ：神戸大学のロゴを作りたい人向けに色情報だけ置いておきます
   const red = color(196, 0, 0);
