d14cbc73ff423b239c787f4e7691e3d4d7cb66c6
A-Yanagi <aoi.yana79km@gmail.com>
2021-10-18 23:17:49 +0900

    change section2-q2
    うまくいってない。

--- section2-q2/sketch.js
+++ section2-q2/sketch.js
@@ -5,7 +5,28 @@ function setup() {
   noStroke();
   for(let i = 0; i < 8; i++){
     for(let j = 0; j < 8; j++){
+      console.log(i, j);
+      rect(i * 20, j * 20, 20, 20);
+      if(j % 2 == 1){
+        if(i % 2 == 0){
+        fill(255, 255, 255);
+        ellipse(i * 20 + 10, j * 20 + 10, 10, 10);
+        }
+        else{
+        fill(169, 169, 169)
+        }
+      }
+      else{
+        if(i % 2 == 1){
+        fill(255, 255, 255);
+        ellipse(i * 20 + 10, j * 20 + 10, 10, 10);
+        }
+        else{
+        fill(169, 169, 169)
+        }
+      }
+    }
+
       // BLANK[1] (hint: rectのx座標は size * i, y座標はsize * j)
     }
   }
-}
