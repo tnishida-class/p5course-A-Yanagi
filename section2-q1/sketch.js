ffaecb16696255741e748e48e858223e423ba14b
A-Yanagi <aoi.yana79km@gmail.com>
2021-10-19 15:13:05 +0900

    change section2-q1
    訂正

--- section2-q1/sketch.js
+++ section2-q1/sketch.js
@@ -1,15 +1,16 @@
 // 小手調べ
 function setup(){
   createCanvas(200, 200)
-  for(let i = 0; i < 11; i++){
+  for(let i = 0; i < 10; i++){
+  let r = (i + 1) * 15;
   noFill();
-  ellipse(100, 100, i * 15)
   if(i < 5){
     stroke(0, 0, 255);
   }
   else{
     stroke(255, 0, 0);
   }
+  ellipse(100, 100, r)
     // BLANK[1]
   }
 }
