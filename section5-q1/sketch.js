299cd01b4220463b11cf80e6b2de54b29a2e804b
A-Yanagi <aoi.yana79km@gmail.com>
2021-11-15 22:43:27 +0900

    change section5-q1

--- section5-q1/sketch.js
+++ section5-q1/sketch.js
@@ -4,15 +4,16 @@
 function setup(){
   createCanvas(400, 400);
   background(255);
-  balloon("I love keyakizaka46");
+  balloon("I love 平手友梨奈.");
 }
 
 function balloon(t){
   let w = textWidth(t);
   let h = textAscent() + textDescent();
-  let p = 2;
-  fill(0);
+  let p = 5;
+  fill(152, 251, 152);
   rect(0, 0, w + p * 2, h + p * 2);
-  fill(255);
+  triangle(w + p * 2 - 4, h + p * 2,  w + p * 2, h + p * 2 - 4, w + p * 2 + 5, h + p * 2 + 5)
+  fill(0);
   text(t, p, h + p);
 }
